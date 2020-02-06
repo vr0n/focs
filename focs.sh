60a680f34ef13c7a98ba61a609458e9c63e6ef9c
Trevon <trevon@debzone.simple.net>
2020-02-05 19:30:20 -0500

    Added afl++ qemu build system. Autofuzz is broken. Install needs to be entirely reworked. Clean needs to be reworked

--- focs.sh
+++ focs.sh
@@ -80,9 +80,6 @@ focs_install() {
 			sudo mkdir /usr/share/focs/
 		fi
 
-		sudo mkdir /usr/share/focs/firmware-library/
-  		sudo cp -r $(sudo find / -path "*afl" 2>&- ) /usr/share/focs/
-
 		clear
 
 		case $DESK in
@@ -110,7 +107,8 @@ focs_install() {
 					clear
 					echo -e "${INFO}Well, it was worth a shot...${NC}" && exit 1
 				else 
-					{ sudo apt install -y git wget python flex coreutils binwalk qemu-user libtool wget python build-essential libtool-bin bison libglib2.0-dev libglib2.0 2>&- && echo -e "${INFO}Installing dependencies...${NC}"; } || { echo -e "${ERROR}Uh oh... issue installing dependencies....${NC}" && exit 1; }
+					# May need libc6-dev-i386
+					{ sudo apt install -y git wget libpixman-1-dev python flex coreutils binwalk qemu-user libtool wget python build-essential libtool-bin bison libglib2.0-dev libglib2.0 2>&- && echo -e "${INFO}Installing dependencies...${NC}"; } || { echo -e "${ERROR}Uh oh... issue installing dependencies....${NC}" && exit 1; }
 				fi
 				;;
 			*)
@@ -133,18 +131,6 @@ focs_install() {
 
 		THISDIR="$(echo $PWD)"
 
-		# Grab latest version of AFL
-		# Commenting this out to host my own version of afl and qemu
-		# or, rather, an unchanging version
-		#{ wget http://lcamtuf.coredump.cx/afl/releases/afl-latest.tgz && echo -e "${INFO}Grabbing the latest version of AFL from Michal... Thanks, Mr. Zalewski!${NC}"; } || { echo -e "${ERROR}Whoops... Problem grabbing the latest AFL! Google lcamtuf to find out why!${NC}" && exit 1; }
-
-
-		# Unpack it
-		#{ tar -xvf afl-latest.tgz && echo -e "${INFO}Unpacking the tarball of AFL${NC}"; } || { echo -e "${RED}Issue unpacking the tarball... Could be an issue with the script?${NC}" && exit 1; }
-
-		# This is to future proof the script (in case the latest version changes)
-		#rm afl-latest.tgz
-		#mv afl* afl/
 		cd afl/ #clever, huh?
 
 		echo -e "${INFO}If you see this, everything is going fine so far....${NC}" || { echo -e "${ERROR}Yikes... If you see this there was a very terrible system error...${NC}" && exit 1; }
@@ -158,69 +144,236 @@ focs_install() {
 
 		# Decided to host my own version of QEMU
 		# So I've commented out the lines to grab a new copy
-		VERSION="2.10.0"
-		QEMU_URL="http://download.qemu-project.org/qemu-${VERSION}.tar.xz"
-		QEMU_SHA384="68216c935487bc8c0596ac309e1e3ee75c2c4ce898aab796faa321db5740609ced365fedda025678d072d09ac8928105"
+		# VERSION="2.10.0"
+		# QEMU_URL="http://download.qemu-project.org/qemu-${VERSION}.tar.xz"
+		# QEMU_SHA384="68216c935487bc8c0596ac309e1e3ee75c2c4ce898aab796faa321db5740609ced365fedda025678d072d09ac8928105"
 
-		# Dealing with QEMU now
-		if [ ! "`uname -s`" = "Linux" ]; then
-		  echo -e "${ERROR}QEMU instrumentation is supported only on Linux.${NC}" && exit 1
-		fi
-
-		if [ ! -f "patches/afl-qemu-cpu-inl.h" -o ! -f "../config.h" ]; then
-		  echo -e "${ERROR}Key files not found - wrong working directory?${NC}" && exit 1
-		fi
-
-		if [ ! -f "../afl-showmap" ]; then
-		  echo -e "${ERROR}../afl-showmap not found - compile AFL first!${NC}" && exit 1
-		fi
-
-		ARCHIVE="`basename -- "$QEMU_URL"`"
-		CKSUM=`sha384sum -- "$ARCHIVE" 2>/dev/null | cut -d' ' -f1`
-
-		if [[ ! "$CKSUM" = "$QEMU_SHA384" ]]; then
-
-		  echo -e "${RED}[*] Downloading QEMU ${VERSION} from the web...${NC}"
-		  rm -f "$ARCHIVE"
-		  wget -O "$ARCHIVE" -- "$QEMU_URL" || exit 1
-
-		  CKSUM=`sha384sum -- "$ARCHIVE" 2>/dev/null | cut -d' ' -f1`
-
-		fi
-
-		if [[ "$CKSUM" = "$QEMU_SHA384" ]]; then
-
-		  echo -e "${INFO}[+] Cryptographic signature on $ARCHIVE checks out.${NC}"
-
-		else
-
-		  echo -e "${ERROR}[-] Error: signature mismatch on $ARCHIVE (perhaps download error?).${NC}"
-		  exit 1
-
-		fi
-
-		echo -e "${INFO}[*] Uncompressing archive (this will take a while)...${NC}"
-
-		rm -rf "qemu-${VERSION}" || exit 1
-		tar xf "$ARCHIVE" || exit 1
-
-		cd qemu-*/ || exit 1
-
-		echo -e "${INFO}[*] Applying patches..."
+		
 		# Reference https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=914218
 		# TODO: This may be debian specific and we need to add a check
-		cp $THISDIR/configs/memfd_create.diff ../patches/
+		# cp $THISDIR/configs/memfd_create.diff ../patches/
+		# patch -p1 <../patches/elfload.diff || exit 1
+		# patch -p1 <../patches/cpu-exec.diff || exit 1
+		# patch -p1 <../patches/syscall.diff || exit 1
+		# patch -p1 <../patches/memfd_create.diff || exit 1
+    VERSION="3.1.1"
+    QEMU_URL="http://download.qemu-project.org/qemu-${VERSION}.tar.xz"
+    QEMU_SHA384="28ff22ec4b8c957309460aa55d0b3188e971be1ea7dfebfb2ecc7903cd20cfebc2a7c97eedfcc7595f708357f1623f8b"
 
-		patch -p1 <../patches/elfload.diff || exit 1
-		patch -p1 <../patches/cpu-exec.diff || exit 1
-		patch -p1 <../patches/syscall.diff || exit 1
-		patch -p1 <../patches/memfd_create.diff || exit 1
+    echo "================================================="
+    echo "AFL binary-only instrumentation QEMU build script"
+    echo "================================================="
+    echo
 
-		echo -e "${INFO}Patching done.${NC}"
+    echo "[*] Performing basic sanity checks..."
 
-		cd $THISDIR
-	fi
+    if [ ! "`uname -s`" = "Linux" ]; then
 
+      echo "[-] Error: QEMU instrumentation is supported only on Linux."
+      exit 0
+
+    fi
+
+    if [ ! -f "patches/afl-qemu-cpu-inl.h" -o ! -f "../config.h" ]; then
+
+      echo "[-] Error: key files not found - wrong working directory?"
+      exit 1
+
+    fi
+
+    if [ ! -f "../afl-showmap" ]; then
+
+      echo "[-] Error: ../afl-showmap not found - compile AFL first!"
+      exit 1
+
+    fi
+
+
+    for i in libtool wget python automake autoconf sha384sum bison iconv; do
+
+      T=`which "$i" 2>/dev/null`
+
+      if [ "$T" = "" ]; then
+
+        echo "[-] Error: '$i' not found, please install first."
+        exit 1
+
+      fi
+
+    done
+
+    if [ ! -d "/usr/include/glib-2.0/" -a ! -d "/usr/local/include/glib-2.0/" ]; then
+
+      echo "[-] Error: devel version of 'glib2' not found, please install first."
+      exit 1
+
+    fi
+
+    if echo "$CC" | grep -qF /afl-; then
+
+      echo "[-] Error: do not use afl-gcc or afl-clang to compile this tool."
+      exit 1
+
+    fi
+
+    echo "[+] All checks passed!"
+
+    ARCHIVE="`basename -- "$QEMU_URL"`"
+
+    CKSUM=`sha384sum -- "$ARCHIVE" 2>/dev/null | cut -d' ' -f1`
+
+    if [ ! "$CKSUM" = "$QEMU_SHA384" ]; then
+
+      echo "[*] Downloading QEMU ${VERSION} from the web..."
+      rm -f "$ARCHIVE"
+      OK=
+      while [ -z "$OK" ]; do
+        wget -c -O "$ARCHIVE" -- "$QEMU_URL" && OK=1
+      done
+
+      CKSUM=`sha384sum -- "$ARCHIVE" 2>/dev/null | cut -d' ' -f1`
+
+    fi
+
+    if [ "$CKSUM" = "$QEMU_SHA384" ]; then
+
+      echo "[+] Cryptographic signature on $ARCHIVE checks out."
+
+    else
+
+      echo "[-] Error: signature mismatch on $ARCHIVE (perhaps download error?), removing archive ..."
+      rm -f "$ARCHIVE"
+      exit 1
+
+    fi
+
+    echo "[*] Uncompressing archive (this will take a while)..."
+
+    rm -rf "qemu-${VERSION}" || exit 1
+    tar xf "$ARCHIVE" || exit 1
+
+    echo "[+] Unpacking successful."
+
+    if [ -n "$HOST" ]; then
+      echo "[+] Configuring host architecture to $HOST..."
+      CROSS_PREFIX=$HOST-
+    else
+      CROSS_PREFIX=
+    fi
+
+    echo "[*] Configuring QEMU for $CPU_TARGET..."
+
+    ORIG_CPU_TARGET="$CPU_TARGET"
+
+    test "$CPU_TARGET" = "" && CPU_TARGET="`uname -m`"
+    test "$CPU_TARGET" = "i686" && CPU_TARGET="i386"
+
+    cd qemu-$VERSION || exit 1
+
+    echo "[*] Applying patches..."
+
+    patch -p1 <../patches/elfload.diff || exit 1
+    patch -p1 <../patches/cpu-exec.diff || exit 1
+    patch -p1 <../patches/syscall.diff || exit 1
+    patch -p1 <../patches/translate-all.diff || exit 1
+    patch -p1 <../patches/tcg.diff || exit 1
+    patch -p1 <../patches/i386-translate.diff || exit 1
+    patch -p1 <../patches/arm-translate.diff || exit 1
+    patch -p1 <../patches/i386-ops_sse.diff || exit 1
+    patch -p1 <../patches/i386-fpu_helper.diff || exit 1
+    patch -p1 <../patches/softfloat.diff || exit 1
+
+    echo "[+] Patching done."
+
+    if [ "$STATIC" = "1" ]; then
+
+      CFLAGS="-O3 -ggdb" ./configure --disable-bsd-user --disable-guest-agent --disable-strip --disable-werror \
+        --disable-gcrypt --disable-debug-info --disable-debug-tcg --disable-tcg-interpreter \
+        --enable-attr --disable-brlapi --disable-linux-aio --disable-bzip2 --disable-bluez --disable-cap-ng \
+        --disable-curl --disable-fdt --disable-glusterfs --disable-gnutls --disable-nettle --disable-gtk \
+        --disable-rdma --disable-libiscsi --disable-vnc-jpeg --enable-kvm --disable-lzo --disable-curses \
+        --disable-libnfs --disable-numa --disable-opengl --disable-vnc-png --disable-rbd --disable-vnc-sasl \
+        --disable-sdl --disable-seccomp --disable-smartcard --disable-snappy --disable-spice --disable-libssh2 \
+        --disable-libusb --disable-usb-redir --disable-vde --disable-vhost-net --disable-virglrenderer \
+        --disable-virtfs --disable-vnc --disable-vte --disable-xen --disable-xen-pci-passthrough --disable-xfsctl \
+        --enable-linux-user --disable-system --disable-blobs --disable-tools \
+        --target-list="${CPU_TARGET}-linux-user" --static --disable-pie --cross-prefix=$CROSS_PREFIX || exit 1
+
+    else
+
+      # --enable-pie seems to give a couple of exec's a second performance
+      # improvement, much to my surprise. Not sure how universal this is..
+      
+      CFLAGS="-O3 -ggdb" ./configure --disable-system \
+        --enable-linux-user --disable-gtk --disable-sdl --disable-vnc \
+        --target-list="${CPU_TARGET}-linux-user" --enable-pie --enable-kvm $CROSS_PREFIX || exit 1
+
+    fi
+
+    echo "[+] Configuration complete."
+
+    echo "[*] Attempting to build QEMU (fingers crossed!)..."
+
+    make -j `nproc` || exit 1
+
+    echo "[+] Build process successful!"
+
+    echo "[*] Copying binary..."
+
+    cp -f "${CPU_TARGET}-linux-user/qemu-${CPU_TARGET}" "../../afl-qemu-trace" || exit 1
+
+    cd ..
+    ls -l ../afl-qemu-trace || exit 1
+
+    echo "[+] Successfully created '../afl-qemu-trace'."
+
+    if [ "$ORIG_CPU_TARGET" = "" ]; then
+
+      echo "[*] Testing the build..."
+
+      cd ..
+
+      make >/dev/null || exit 1
+
+      gcc test-instr.c -o test-instr || exit 1
+
+      unset AFL_INST_RATIO
+
+      echo 0 | ./afl-showmap -m none -Q -q -o .test-instr0 ./test-instr || exit 1
+      echo 1 | ./afl-showmap -m none -Q -q -o .test-instr1 ./test-instr || exit 1
+
+      rm -f test-instr
+
+      cmp -s .test-instr0 .test-instr1
+      DR="$?"
+
+      rm -f .test-instr0 .test-instr1
+
+      if [ "$DR" = "0" ]; then
+
+        echo "[-] Error: afl-qemu-trace instrumentation doesn't seem to work!"
+        exit 1
+
+      fi
+
+      echo "[+] Instrumentation tests passed. "
+      echo "[+] All set, you can now use the -Q mode in afl-fuzz!"
+
+      cd qemu_mode || exit 1
+
+    else
+
+      echo "[!] Note: can't test instrumentation when CPU_TARGET set."
+      echo "[+] All set, you can now (hopefully) use the -Q mode in afl-fuzz!"
+
+    fi
+
+    echo "[+] Building libcompcov ..."
+    make -C libcompcov && echo "[+] libcompcov ready"
+    echo "[+] Building unsigaction ..."
+    make -C unsigaction && echo "[+] unsigaction ready"
+    echo "[+] All done for qemu_mode, enjoy!"
+    fi
 	echo -e "${INFO}${NC}"
 	echo -e "${INFO}################################################${NC}"
 	echo -e "${INFO}     All done with Dependencies and AFL make.   ${NC}"
@@ -358,7 +511,7 @@ auto-fuzz () {
 	fi
 
 	# TODO: No save and not continue
-	if [[ -z $(ls out/*) ]] ; then
+	if [[ -z $(ls $FOCS/$4/out/*) ]] ; then
 	   echo -e "${INFO} Would you like to continue your previous job? (Y/n)"
 	   read OPT
 
@@ -422,7 +575,9 @@ extract () {
 	sudo cp $file /usr/share/focs/ || { echo -e "${ERROR}Couldn't move the file to the /usr/share/focs directory... check that it exists already.${NC}" && exit 1; };
 
 	cd /usr/share/focs/ || { echo -e "${ERROR}Issue jumping into the /usr/share/focs directory. Check that it exists.${NC}" && exit 1; };
-	sudo binwalk -e /usr/share/focs/$file || { echo -e "${ERROR}Unfortunately, binwalk threw an issue... This can't be fixed by me, I'm afraid...${NC}" && exit 1; }; 
+	sudo binwalk -e /usr/share/focs/$(basename "$file")  || { echo -e "${ERROR}Unfortunately, binwalk threw an issue... This can't be fixed by me, I'm afraid...${NC}" && exit 1; }; 
+
+	file=$(basename "$file")
 
 	find _*/ -name 'bin'
 
